2207d93e76e9bb0fb9502ff70a67cc2a5097f8bf
Thomas Forselius <forselius.thomas@gmail.com>
2022-06-07 19:21:52 +0200

    added function to add player to list

--- run.py
+++ run.py
@@ -13,10 +13,12 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('players')
 
 players_sheet = SHEET.worksheet('players')
+#defining variable players_sheet to fetch data from players sheet
 admin_sheet = SHEET.worksheet('admin')
+#defining variable players_sheet to fetch data from admins sheet
 
-players = players_sheet.get_all_values()
-admins = admin_sheet.get_all_values()
+players = players_sheet.get_all_values() #gets all values from players sheet
+admins = admin_sheet.get_all_values() #gets all values from admin sheet
 print(players)
 print(admins)
 
@@ -25,15 +27,27 @@ def show_players(type):
     Gets players or admins from sheet
     """
 
+def validate_data(type, data):
+    """
+    Validates a certain type of data based on what type it is
+    Numbers ar checked for int type
+    Strings are checked for str type
+    """
+
 
 def new_player():
     """
     Creates a new instance of regular player
     """
-    print("\n***** Enter a player name: *****")
-    f_name = input("\nFirst name: ")
+    print("***** Create a player *****")
+    print("You must enter information about the player you wish to create.\n")
+    print("These are the required credentials: ")
+    print("Admin or Regular player, First name, Last name, Age and Email.\n")
+
+    type = input("Admin or regular player?\n")
+    f_name = input("First name: \n")
         # check function comes here to check if age is only str and not int
-    l_name = input("Last name: ")
+    l_name = input("Last name: \n")
         # check function comes here to check if age is only str and not int
     age = input("Age: ")
         # check function comes here to check if age is only int and not str
@@ -46,11 +60,16 @@ def new_player():
     print(f"Age: {age}")
     print(f"Email: {email}")
     print(f"\n")
+    player_data = [f_name, l_name, age, email]
+    print("Adding player to list...\n")
+    players_sheet.append_row(player_data)
+    print("Player added to list!")
+
 
 
 def main():
-    print("hello terminal")
+    print("*** Welcome to player database control center ***\n\n")
     new_player()
 
 
-#main()
+main()
