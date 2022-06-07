c1177260f072b5a75aba3a23450b90507c8f718c
Thomas Forselius <forselius.thomas@gmail.com>
2022-06-07 18:15:44 +0200

    added scope and credentials and got data from sheet

--- run.py
+++ run.py
@@ -1,6 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('players')
+
+players_sheet = SHEET.worksheet('players')
+admin_sheet = SHEET.worksheet('admin')
+
+players = players_sheet.get_all_values()
+admins = admin_sheet.get_all_values()
+print(players)
+print(admins)
+
+def show_players(type):
+    """
+    Gets players or admins from sheet
+    """
 
 
 def new_player():
@@ -30,4 +53,4 @@ def main():
     new_player()
 
 
-main()
+#main()
